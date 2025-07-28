11d44b52cc0c8ef9d6f71aa2a0a880e0c4c07215
Najeed-31 <najeedmubasher31@gmail.com>
2025-07-28 06:56:20 -0400

    Add files via upload

--- Assignment-01/Assignment-01/Program.cs
+++ Assignment-01/Assignment-01/Program.cs
@@ -33,6 +33,12 @@
                 case 8:
                     Task8();
                     break;
+                case 9:
+                    Task9();
+                    break;
+                default:
+                    Console.WriteLine("Invalid Input.");
+                    break;
             }
 
         }
@@ -164,6 +170,30 @@
                 temp = first;
                 first = second;
                 second = temp + second;
+            }
+        }
+
+        static void Task9()
+        {
+            int num;
+            bool isPrime=true;
+            Console.WriteLine("Enter a number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            for(int i=2;i<=num/2;i++)
+            {
+                if (num%i==0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime==false)
+            {
+                Console.WriteLine(num + " is not a prime number.");
+            }
+            else
+            {
+                Console.WriteLine(num + " is a prime number");
             }
         }
     }
